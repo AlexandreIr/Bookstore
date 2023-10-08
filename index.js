07c740e57f5ede3c0acd56f18c095b9441fc1adb
Alexandre Fernandes <alex.silva250@hotmail.com>
2023-10-08 13:45:03 -0300

    organization

--- index.js
+++
@@ -1,7 +0,0 @@
-const Book = require("./Book");
-const Magazines = require("./Magazines");
-const Order = require("./Order");
-
-const book= new Book('O segredo', 'Larry Bird', 365, 'Ficction', 65, 40);
-const magazine= new Magazines('Brasil Paralelo', 'Abril', 'Política', false, 39.99, 500)
-
